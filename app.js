b4c46ed4824b7ac3d54c87a16a83248db56211f6
unknown <baudinopierreolivier@gmail.com>
2023-01-25 10:23:53 +0100

    add tools and src of pb will be seen

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("test");
+// console.log("test");
+//!Recup DOM
+var player = document.getElementById("player");
+
+//*---------------------------------------
+//*Tools:
+//link : https://webdesign.tutsplus.com/tutorials/build-a-custom-html-music-player-using-javascript-and-the-web-audio-api--cms-93300
+// console.log(player.currentTime);
+// console.log(player.currentSrc);
+// console.log(player.duration);
+// console.log(player.ended);
+// console.log(player.volume);
+
+// player.play();
+// player.pause();
+
+//*see audioContext
+//! wave sound
+// TODO: read : https://css-tricks.com/making-an-audio-waveform-visualizer-with-vanilla-javascript/
+// TODO : see: https://www.youtube.com/watch?v=ftjZI4mLCoI&list=RDCMUCkjoHfkLEy7ZT4bA2myJ8xA&start_radio=1&rv=ftjZI4mLCoI&t=0
