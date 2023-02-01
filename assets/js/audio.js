398b99c18bbf77e47afdb7510e274ba131238010
unknown <baudinopierreolivier@gmail.com>
2023-02-01 23:31:37 +0100

    test confection walls in game

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -2,7 +2,8 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const audioContext = new AudioContext();
-// var currentBuffer = null;
+const canvas = document.getElementById("screen-canvas");
+const ctx = canvas.getContext("2d");
 
 //**link mp3.infos to audioContext */
 const visualizeAudio = (url) => {
@@ -11,25 +12,17 @@ const visualizeAudio = (url) => {
     .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
     .then((audioBuffer) => draw(normalizeData(filterData(audioBuffer))));
 };
-// console.log(visualizeAudio);
 //* filter fetch data*/
 const filterData = (audioBuffer) => {
   const rawData = audioBuffer.getChannelData(0);
-  const samples = 70;
+  const samples = 50;
   const blockSize = Math.floor(rawData.length / samples);
   const filteredData = [];
   for (let i = 0; i < samples; i++) {
-    // filterData.push(rawData[i * blockSize]);
-    let blockStart = blockSize * i;
-    let sum = 0;
-    for (let j = 0; j < blockSize; j++) {
-      sum = sum + Math.abs(rawData[blockStart + j]);
-    }
-    filteredData.push(sum / blockSize);
+    filteredData.push(rawData[i * blockSize]);
   }
   return filteredData;
 };
-// console.log(filterData);
 //**if no sound, min=1 */
 const normalizeData = (filteredData) => {
   const multiplier = Math.pow(Math.max(...filteredData), -1);
@@ -37,46 +30,78 @@ const normalizeData = (filteredData) => {
 };
 //*inject data.infos in html */
 const draw = (normalizedData) => {
-  const canvas = document.getElementById("screen-canvas");
-  //   console.log(canvas);
   const dpr = window.devicePixelRatio || 1;
-  //   console.log(dpr);
   const padding = 20;
-  //   console.log(canvas.height);
-  console.log(canvas.offsetHeight);
   canvas.width = canvas.offsetWidth * dpr;
   canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
-  const ctx = canvas.getContext("2d");
+
   ctx.scale(dpr, dpr);
-  //   console.log(ctx);
   ctx.translate(0, 250 + padding);
 
-  //   console.log(normalizeData);
   //*Draw line segment/
-  const width = canvas.offsetWidth / normalizedData.length;
-  //   console.log(normalizeData);
+  // const width = canvas.offsetWidth / normalizedData.length;
   for (let i = 0; i < normalizedData.length; i++) {
-    const x = width * i;
+    // const x = width * i;
     let height = normalizedData[i] * canvas.offsetHeight - padding;
-    if (height < 0) {
-      height = 0;
-    } else if (height > canvas.offsetHeight - padding) {
+    // console.log(height);
+    if (height > 0) {
+      height = -height;
+    }
+    if (height > canvas.offsetHeight - padding) {
       height = canvas.offsetHeight - padding;
     }
-    drawLineSegment(ctx, x, height, width, (i + 1) % 2);
+    // console.log(ctx);
+    // drawLineSegment(ctx, x, height, (i + 1) % 2);
   }
 };
 //*draw lines/
-const drawLineSegment = (ctx, x, height, width, isEven) => {
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = "#fff";
-  ctx.beginPath();
-  height = isEven ? -height : -height;
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, height);
-  ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
-  ctx.lineTo(x + width, 0);
-  ctx.stroke();
-};
+// const drawLineSegment = (ctx, x, height, isEven) => {
+//   ctx.lineWidth = 5;
+//   ctx.strokeStyle = "#fff";
+//   ctx.beginPath();
+//   height = isEven ? height : height;
+//   ctx.moveTo(x, 0);
+//   ctx.lineTo(x, height);
+//   ctx.stroke();
+// };
 
-visualizeAudio("./list/tropical-summer-music-112842.mp3");
+// visualizeAudio("./list/tropical-summer-music-112842.mp3");
+
+//!-----------------------------------------------------
+//*Game
+// general settings
+const speed = 1;
+const cTenth = canvas.width / 10;
+
+let index = 0,
+  flyHeight,
+  pipes = [];
+
+// pipe settings
+const pipeWidth = 10;
+const pipeHeight = 50;
+const pipeGap = 5;
+const pipeLoc = 10;
+
+var position;
+
+function drawWall() {
+  // !Experimental:
+  // ctx.reset();
+  // *black for reset
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // *blue for wall
+  ctx.fillStyle = "blue";
+  position--;
+  ctx.fillRect(position, 110, pipeWidth, pipeHeight);
+  // console.log(position);
+}
+function launch() {
+  position = 260;
+  setInterval(drawWall, 1000);
+}
+
+// start game
+const play = document.getElementById("play");
+play.addEventListener("click", launch);
