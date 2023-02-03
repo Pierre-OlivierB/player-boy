69a041f084d9e6e0f52569f736060eaf3137f6d1
unknown <baudinopierreolivier@gmail.com>
2023-02-03 10:11:29 +0100

    add position and user interaction on volume

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,37 @@
 //! wave sound
 // TODO: read : https://css-tricks.com/making-an-audio-waveform-visualizer-with-vanilla-javascript/
 // TODO : see: https://www.youtube.com/watch?v=ftjZI4mLCoI&list=RDCMUCkjoHfkLEy7ZT4bA2myJ8xA&start_radio=1&rv=ftjZI4mLCoI&t=0
+
+// *sound control
+const soundCtrlPlus = document.getElementById("plus-btn");
+const soundCtrlMinus = document.getElementById("minus-btn");
+const countVol = document.getElementById("cnt-sound");
+console.log(countVol.innerText);
+var vol = player.volume;
+// console.log(soundCtrl);
+
+function soundPlus() {
+  if (vol === 1 || vol > 1) {
+    vol = 1;
+    console.log("no plus");
+  } else if (vol < 1) {
+    vol = vol + 0.01;
+  }
+  countVol.innerText = Math.ceil(vol * 100);
+
+  console.log(vol);
+}
+function soundMinus() {
+  if (vol === 0 || vol <= 0.01) {
+    vol = 0;
+
+    console.log("no minus");
+  } else if (vol > 0.01) {
+    vol = vol - 0.01;
+  }
+  countVol.innerText = Math.ceil(vol * 100);
+  console.log(vol);
+}
+
+soundCtrlPlus.addEventListener("click", soundPlus);
+soundCtrlMinus.addEventListener("click", soundMinus);
