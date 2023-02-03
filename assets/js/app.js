d9ca80fcb83062306c269b52bbfe75c981fbb22c
unknown <baudinopierreolivier@gmail.com>
2023-02-03 10:33:49 +0100

    reactions on btn next and prev

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,36 +19,53 @@
 // TODO: read : https://css-tricks.com/making-an-audio-waveform-visualizer-with-vanilla-javascript/
 // TODO : see: https://www.youtube.com/watch?v=ftjZI4mLCoI&list=RDCMUCkjoHfkLEy7ZT4bA2myJ8xA&start_radio=1&rv=ftjZI4mLCoI&t=0
 
+//*--------------------------------------------------------------
 // *sound control
 const soundCtrlPlus = document.getElementById("plus-btn");
 const soundCtrlMinus = document.getElementById("minus-btn");
 const countVol = document.getElementById("cnt-sound");
-console.log(countVol.innerText);
+// console.log(countVol.innerText);
 var vol = player.volume;
 // console.log(soundCtrl);
 
 function soundPlus() {
   if (vol === 1 || vol > 1) {
     vol = 1;
-    console.log("no plus");
+    // console.log("no plus");
   } else if (vol < 1) {
     vol = vol + 0.01;
   }
   countVol.innerText = Math.ceil(vol * 100);
 
-  console.log(vol);
+  //   console.log(vol);
 }
 function soundMinus() {
   if (vol === 0 || vol <= 0.01) {
     vol = 0;
 
-    console.log("no minus");
+    // console.log("no minus");
   } else if (vol > 0.01) {
     vol = vol - 0.01;
   }
   countVol.innerText = Math.ceil(vol * 100);
-  console.log(vol);
+  //   console.log(vol);
 }
 
 soundCtrlPlus.addEventListener("click", soundPlus);
 soundCtrlMinus.addEventListener("click", soundMinus);
+
+//*-------------------------------------------------------
+//* next music
+const nextMusic = document.getElementById("next-btn");
+const prevMusic = document.getElementById("before-btn");
+// console.log(nextMusic, prevMusic);
+
+function nxtMsc() {
+  console.log("next music");
+}
+function prvMsc() {
+  console.log("previous music");
+}
+
+nextMusic.addEventListener("click", nxtMsc);
+prevMusic.addEventListener("click", prvMsc);
